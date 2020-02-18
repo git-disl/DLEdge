8d6bd01675529df5cd724eb405835019c622b93f
vicase <vicase98@correo.urg.es>
2020-02-17 21:51:04 -0500

    Implementation of majority vote on graph cliques algorithm for ensemble

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import cv2
 import base64
 import numpy as np
+import networkx as nx
 
 
 def base64tocv2(s):
@@ -85,3 +86,122 @@ class DetectedObject(object):
         self.class_id = class_id
         self.name = class_names[class_id]
         self.confidence = confidence
+
+#-------------------------------------------------
+# Helper functions for vote clique
+def get_cliques(models_predictions):
+    #Creat the graph
+    G = nx.Graph()
+
+    # Index of the node
+    i = 0
+    # Index of the model (to track model-prediction)
+    j = 0
+
+    for model in models_predictions:
+        j += 1
+        for object in models_predictions[model]:
+            G.add_node(i, bbox=object['bbox'])
+            G.nodes[i]['class'] = object['class']
+            G.nodes[i]['score'] = object['score']
+            G.nodes[i]['model'] = j
+            i += 1
+
+    #Now we have all the objects detected in a graph
+    # Create clique for each node and save it as list
+    # cliques[i] will contain a set of vertices
+    cliques_aux = nx.find_cliques(G)
+    cliques_aux  = list(cliques_aux)
+    cliques = []
+    for cl in cliques_aux:
+        cliques.append({cl[0]})
+
+    # Now we have to build the edges
+    # Also store them in array to sort
+    #In edges[i], first and second number are vertices
+    # the third number is IoU
+    edges = []
+    n = len(list(G.nodes()))
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            #If iou is not zero, build edge.
+            # intersection_over_union expects a dictionary
+            iou = intersection_over_union(G.nodes[i],G.nodes[j])
+            if iou != 0:
+                G.add_edge(i,j, iou = iou)
+                edges.append([i,j,iou])
+
+
+    # Sort the edges by weight (IoU) in descending order
+    edges.sort(key = lambda x: x[2], reverse=True)
+
+    # merge the two cliques the edges would connect if
+    # their sets of vertex colors are disjoint
+    for edge in edges:
+        #Find the clique for each vertex
+        i = -1
+        j = -1
+        for k in range(len(cliques)):
+            #Recall: cliques[k] contains indices of vertices in that clique
+            if(edge[0] in cliques[k]):
+                i = k
+            if(edge[1] in cliques[k]):
+                j = k
+
+        #Compute set of colors (model) for each clique
+        colorsClique1 = set()
+        colorsClique2 = set()
+        for vertex in cliques[i]:
+            color = G.nodes[vertex]['model']
+            colorsClique1.add(color)
+
+        for vertex in cliques[j]:
+            color = G.nodes[vertex]['model']
+            colorsClique2.add(color)
+
+        intersection = colorsClique1.intersection(colorsClique2)
+        # If disjoint of colors, merge
+        if(len(intersection) == 0):
+            cliques[i] = cliques[i].union(cliques[j])
+            cliques.pop(j)
+
+    # return the remaining cliques
+    return cliques, G
+
+# @param predictions: array with dictionaries. Each dictionary is a prediction,
+# with the same structure as in detect_objects_response
+def vote_cliques(cliques, G, voting_threshold = 0.5):
+    # Counting the number of diferent models (colors) in the clique gives us
+    # how many models vote for a clique
+    cliqueVotes = []
+    totalColors = set()
+    for cl in cliques:
+        colors = set()
+        for node in cl:
+            color = G.nodes[node]['model']
+            colors.add(color)
+            totalColors.add(color)
+        cliqueVotes.append(len(colors))
+
+    predictions = []
+    totalColors = len(totalColors)
+    for i in range(len(cliques)):
+        percentVote = cliqueVotes[i] /  totalColors
+        if(percentVote >= voting_threshold):
+            # Cliques is an array of sets. Each set is a clique with
+            # the indices of the nodes in the clique
+            # Get the node (box prediction) with highest score in each clique
+            prediction = {'score': -1}
+            for indexNode in cliques[i]:
+                box = G.nodes[indexNode]
+                if(box['score'] > prediction['score']):
+                    prediction = box
+            predictions.append(prediction)
+
+
+    return predictions
+
+# End of helper functions for majorty voting on graph cliques
+#---------------------------------------------------
